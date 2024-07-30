1a9c194e3ed1fd048acc2f0bc32f3fa6f9be1ba1
Henao13 <shenaor4@eafit.edu.co>
2024-07-30 08:04:04 -0500

    Actualización views

--- movie/views.py
+++ movie/views.py
@@ -6,4 +6,4 @@ def home(request):
     return render(request, 'home.html', {'name': 'Santiago Henao R'})
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'about.html')
